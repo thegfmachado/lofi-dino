350207b35e71b2a82bfa4e8683252267f025a820
thegfmachado <gabriel.ferreira.machado@hotmail.com>
2024-01-24 12:04:43 -0300

    chore: initial structures

---
+++ .eslintrc.cjs
@@ -0,0 +1,34 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
+    'plugin:react/jsx-runtime',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/no-explicit-any': 'error',
+    'sort-keys': 'error',
+  },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json', './tsconfig.node.json'],
+    tsconfigRootDir: __dirname,
+  },
+  settings: {
+    react: {
+      version: "detect"
+    },
+  },
+}
---
+++ tailwind.config.js
@@ -0,0 +1,99 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      animation: {
+        text1: 'text1 5s linear 0s infinite',
+        text2: 'text2 6s linear 1s infinite',
+        text3: 'text3 7s linear 2s infinite',
+      },
+      keyframes: {
+        text1: {
+          '0%': {
+            'background-size': '200% 200%',
+            'background-position': 'left center',
+          },
+          '50%': {
+            'background-size': '200% 200%',
+            'background-position': 'right center',
+          },
+          '100%': {
+            'background-size': '200% 200%',
+            'background-position': 'left center',
+            'color': 'white',
+          },
+        },
+        text2: {
+          '0%': {
+            'background-size': '200% 200%',
+            'background-position': 'left center',
+          },
+          '50%': {
+            'background-size': '200% 200%',
+            'background-position': 'right center',
+          },
+          '100%': {
+            'background-size': '200% 200%',
+            'background-position': 'left center',
+            'color': 'white',
+          },
+        },
+        text3: {
+          '0%': {
+            'background-size': '200% 200%',
+            'background-position': 'left center',
+          },
+          '50%': {
+            'background-size': '200% 200%',
+            'background-position': 'right center',
+          },
+          '100%': {
+            'background-size': '200% 200%',
+            'background-position': 'left center',
+            'color': 'white',
+          },
+        },
+      },
+      colors: {
+        'title-1': {
+          'start-color': {
+            100: '#007CF0',
+          },
+          'middle-color': {
+            100: '#7159c1',
+          },
+          'end-color': {
+            100: '#00DFD8',
+          },
+        },
+        'title-2': {
+          'start-color': {
+            200: '#7928CA',
+          },
+          'end-color': {
+            200: '#FF0080',
+          },
+        },
+        'title-3': {
+          'start-color': {
+            300: '#FF4D4D',
+          },
+          'end-color': {
+            300: '#F9CB28',
+          },
+        }
+      }
+    },
+  },
+  plugins: [
+    require('daisyui'),
+  ],
+  daisyui: {
+    themes: ['dracula'],
+  },
+}
+
