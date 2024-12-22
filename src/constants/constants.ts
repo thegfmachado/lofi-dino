3f7c53de2da7667bd28e99e40e4682cb49c4326b
thegfmachado <gabriel.ferreira.machado@hotmail.com>
2024-12-21 21:09:48 -0300

    fix(constants): adjusting audio effects paths

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -3,47 +3,47 @@ import { Bird, Car, CloudRain, Coffee, FireSimple, Keyboard, Snowflake, TreeEver
 export const AUDIO_METADATA = {
   'birds': {
     icon: Bird,
-    src: '../public/audio/birds.mp3',
+    src: '/audio/birds.mp3',
     tip: 'birds',
   },
   'coffee': {
     icon: Coffee,
-    src: '../public/audio/coffee.mp3',
+    src: '/audio/coffee.mp3',
     tip: 'coffee shop',
   },
   'fireplace': {
     icon: FireSimple,
-    src: '../public/audio/fireplace.mp3',
+    src: '/audio/fireplace.mp3',
     tip: 'fireplace',
   },
   'forest': {
     icon: TreeEvergreen,
-    src: '../public/audio/forest.mp3',
+    src: '/audio/forest.mp3',
     tip: 'forest',
   },
   'keyboard': {
     icon: Keyboard,
-    src: '../public/audio/keyboard.mp3',
+    src: '/audio/keyboard.mp3',
     tip: 'keyboard',
   },
   'ocean': {
     icon: Waves,
-    src: '../public/audio/ocean.mp3',
+    src: '/audio/ocean.mp3',
     tip: 'ocean',
   },
   'rain': {
     icon: CloudRain,
-    src: '../public/audio/rain.mp3',
+    src: '/audio/rain.mp3',
     tip: 'rain',
   },
   'snow': {
     icon: Snowflake,
-    src: '../public/audio/snow.mp3',
+    src: '/audio/snow.mp3',
     tip: 'snow',
   },
   'traffic': {
     icon: Car,
-    src: '../public/audio/traffic.mp3',
+    src: '/audio/traffic.mp3',
     tip: 'traffic',
   },
 }
